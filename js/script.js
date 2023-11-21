9c1f7e66955ca4be1ec418d3a2175e8070a9279b
federicaarosio <arosio.fede@gmail.com>
2023-11-21 16:33:33 +0100

    working on eventlistener on button

--- js/script.js
+++ js/script.js
@@ -6,25 +6,37 @@
 //creo una funzione che genera una cella
     //creo elemento per la cella
     //aggiungo evenuali classi all'elemento
-    //se serve lo popolo
-    //lo aggiungo al parent
+
+    //popolo e lo aggiungo al parent
 
 //creo un ciclo che mi genera n celle (in questo caso 100)
-
-//creo un ciclo che genera numeri da 1 a 100 come contenuto della cella
+    //nel ciclo genero numeri da 1 a 100 come contenuto della cella
 
 //toggle per dare e rimuovere il colore alla cella ed emettere msg console con numero cella
 
+const playBtnElement = document.querySelector("button.play");
+console.log(playBtnElement);
+playBtnElement.addEventListener("click", function() {
 
-//parent
+    for (let i = 1 ; i <= 100 ; i++){
+        const currentSquare = generateSquare();
+        const squareContent = i;
+        currentSquare.innerHTML += `<span> ${squareContent} </span>`;
+        gridWrapperElement.appendChild(currentSquare);
+    
+        currentSquare.addEventListener("click", function(){
+            this.classList.toggle('bg-light-blue');
+            console.log(squareContent);
+        });
+    }
+});
+
+
+
+
+
 const gridWrapperElement = document.querySelector("div.grid-wrapper");
 
-for (let i = 1 ; i <= 100 ; i++){
-    const currentSquare = generateSquare();
-    const squareContent = i;
-    currentSquare.innerHTML += `<span> ${squareContent} </span>`;
-    gridWrapperElement.appendChild(currentSquare);
-}
 
 
 
